2038fa5dac35d69b74c1f3871460c6e55c87d046
MalcolmFonseca <malcolmfonseca@hotmail.com>
2026-01-04 11:35:11 -0500

    assignments lists as vue components

---
+++ js/components/Assignment.js
@@ -0,0 +1,11 @@
+export default {
+    template: `<li>
+                    <label>{{ assignment.name }}
+                        <input v-model="assignment.complete" type="checkbox">
+                    </label>
+                </li>`,
+
+    props: {
+        assignment: Object
+    }
+}
