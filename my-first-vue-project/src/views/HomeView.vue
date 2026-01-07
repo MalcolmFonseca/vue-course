1cbd0a72f832f320820ea37cd39f1cebad2141f6
MalcolmFonseca <malcolmfonseca@hotmail.com>
2026-01-07 13:06:05 -0500

    tabbable text area component

--- my-first-vue-project/src/views/HomeView.vue
+++ my-first-vue-project/src/views/HomeView.vue
@@ -1,19 +1,14 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { useStorage } from '@/composables/useStorage';
+import TabbableTextarea from '@/components/TabbableTextarea.vue';
+import { ref } from 'vue';
 
-let food = useStorage('food', 'salad');
-let age = useStorage('age');
+let comment = ref('test value');
 </script>
 
 <template>
   <main>
-    <p>
-      What is your favourite food? <input type="text" v-model="food">
-    </p>
-
-    <p>
-      How old are you? <input type="text" v-model="age">
-    </p>
+    <form>
+      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;"/>
+    </form>
   </main>
 </template>
